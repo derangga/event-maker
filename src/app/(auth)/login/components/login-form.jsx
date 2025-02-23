866016931610c4574cdc96b71aa5c72578e6c30d
Dimas Rangga <derangga1011@gmail.com>
2025-02-23 17:52:58 +0700

    feat: implement google oauth (#5)

--- src/app/(auth)/login/components/login-form.jsx
+++ src/app/(auth)/login/components/login-form.jsx
@@ -3,12 +3,16 @@
 import { addToast, Button, Form, Input } from "@heroui/react";
 import { redirect, useRouter } from "next/navigation";
 import { useActionState, useEffect } from "react";
-import loginAction from "../action/login-action";
+import { loginAction, loginGoogleAction } from "../action/login-action";
 import { GoogleIcon } from "./oauth-button";
 
 export const LoginForm = () => {
   const router = useRouter();
   const [state, formAction, isPending] = useActionState(loginAction, null);
+  const [oauthState, oauthFormAction, isPendingOuth] = useActionState(
+    loginGoogleAction,
+    null
+  );
 
   useEffect(() => {
     if (state?.success) {
@@ -24,56 +28,57 @@ export const LoginForm = () => {
 
   return (
     <main className="flex flex-col w-screen h-screen justify-center items-center">
-      <Form
-        className="w-full max-w-md flex flex-col border rounded-xl p-10 shadow-md gap-4"
-        validationBehavior="native"
-        action={formAction}
-      >
-        <Input
-          isRequired
-          label="Email"
-          labelPlacement="outside"
-          name="email"
-          placeholder="Enter your email"
-          type="email"
-        />
+      <div className="w-full max-w-md flex flex-col border rounded-xl p-10 shadow-md">
+        <Form className="gap-4" validationBehavior="native" action={formAction}>
+          <Input
+            isRequired
+            label="Email"
+            labelPlacement="outside"
+            name="email"
+            placeholder="Enter your email"
+            type="email"
+          />
 
-        <Input
-          isRequired
-          label="Password"
-          labelPlacement="outside"
-          name="password"
-          placeholder="Enter your password"
-          type="password"
-        />
-        <div className="flex gap-2 w-full">
+          <Input
+            isRequired
+            label="Password"
+            labelPlacement="outside"
+            name="password"
+            placeholder="Enter your password"
+            type="password"
+          />
+          <div className="flex gap-2 w-full">
+            <Button
+              variant="flat"
+              className="grow"
+              onPress={() => {
+                router.push("/register");
+              }}
+            >
+              Sign up
+            </Button>
+            <Button
+              color="primary"
+              type="submit"
+              className="grow"
+              isLoading={isPending}
+            >
+              Sign in
+            </Button>
+          </div>
+        </Form>
+        <Form className="mt-4" action={oauthFormAction}>
           <Button
-            variant="flat"
-            className="grow"
-            onPress={() => {
-              router.push("/register");
-            }}
-          >
-            Sign up
-          </Button>
-          <Button
-            color="primary"
+            variant="bordered"
             type="submit"
-            className="grow"
-            isLoading={isPending}
+            fullWidth
+            startContent={<GoogleIcon />}
+            isLoading={isPendingOuth}
           >
-            Sign in
+            Sign in with Google
           </Button>
-        </div>
-        <Button
-          variant="bordered"
-          fullWidth
-          onPress={() => {}}
-          startContent={<GoogleIcon />}
-        >
-          Sign in with Google
-        </Button>
-      </Form>
+        </Form>
+      </div>
     </main>
   );
 };
